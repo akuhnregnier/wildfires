4fe6a918b91afde02e72899827e071c5adea69d2
Alexander Kuhn-Regnier <ahk114@ic.ac.uk>
2019-03-11 17:20:33 +0000

    Added new analysis scripts for cx1 copernicus.

---
+++ data/cx1_scripts/copernicus_analysis.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys
+import os
+sys.path.append(os.path.expanduser(os.path.join(
+    '~', 'Documents', 'wildfires', 'data')))
+# print('path')
+# from pprint import pprint
+# pprint(sys.path)
+# pprint(os.listdir(sys.path[-1]))
+import logging
+logging.basicConfig(level=logging.DEBUG)
+from datasets import CHELSA
+
+# 1680 files
+
+index = int(os.environ['PBS_ARRAY_INDEX'])
+
+print("Value of ARRAY_ID:{:}".format(index))
+CHELSA(slice(index, index+1))
---
+++ data/cx1_scripts/run_copernicus_script.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+#PBS -N copernicus
+#PBS -l select=1:ncpus=1:mem=10gb
+#PBS -l walltime=0:35:00
+#PBS -J 0-1679
+
+# 148 slices to cover all the months
+
+/rds/general/user/ahk114/home/.pyenv/versions/miniconda3-latest/bin/python3 /rds/general/user/ahk114/home/Documents/cx1_scripts/chelsa_analysis.py
