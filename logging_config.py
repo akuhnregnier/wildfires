c9ac150f901e857387cbd028c30d6e31324e32b8
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2019-04-09 21:46:02 +0100

    Added new file handler which logs all exceptions at the root level and below, and another which logs only WARNING levels and above. Changed default levels for the package handlers to INFO. Added some code to test the new behaviour.

--- logging_config.py
+++ logging_config.py
@@ -18,23 +18,63 @@ LOGGING = {
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
-            'level': 'DEBUG',
+            'level': 'INFO',
             'formatter': 'default',
         },
         'file': {
             'class': 'logging.FileHandler',
-            'level': 'DEBUG',
+            'level': 'INFO',
             'formatter': 'default',
             'filename': os.path.join(log_dir, 'log.log',),
             'mode': 'a',
-        }
+        },
+        'root_file': {
+            'class': 'logging.FileHandler',
+            'level': 'DEBUG',
+            'formatter': 'default',
+            'filename': os.path.join(log_dir, 'root_log.log',),
+            'mode': 'a',
+        },
+        'warn_file': {
+            'class': 'logging.FileHandler',
+            'level': 'WARNING',
+            'formatter': 'default',
+            'filename': os.path.join(log_dir, 'warning_log.log',),
+            'mode': 'a',
+        },
     },
     'loggers': {
         'wildfires': {
             'level': 'DEBUG',
             'handlers': ['console', 'file']
         },
+        '': {
+            'level': 'DEBUG',
+            'handlers': ['root_file', 'warn_file']
+        },
     }
 }
 LOGGING['loggers']['__main__'] = LOGGING['loggers']['wildfires']
 
+
+if __name__ == '__main__':
+    import logging
+    import logging.config
+    from logging_tree import printout
+    import cdsapi
+    c = cdsapi.Client()
+    logging.config.dictConfig(LOGGING)
+    logger0 = logging.getLogger('')
+    logger1 = logging.getLogger(__name__)
+    logger2 = logging.getLogger('testing')
+    printout()
+
+    for level, level_str in (
+            (logging.DEBUG, 'DEBUG'),
+            (logging.INFO, 'INFO'),
+            (logging.WARNING, 'WARNING')):
+        for logger, logger_name in (
+                (logger0, 'root logger'),
+                (logger1, '{} logger'.format(__name__)),
+                (logger2, 'testing logger')):
+            logger.log(level, "{} {}".format(level_str, logger_name + ' test'))
