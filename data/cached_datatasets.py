77cfb9a21d22556d3927879dd1ad8f0c743df9f0
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2019-05-07 08:32:44 +0100

    joblib Memory cache testing.

---
+++ data/cached_datatasets.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from joblib import Memory
+from wildfires.data.datasets import HYDE, DATA_DIR
+
+mem = Memory(location=DATA_DIR, verbose=2)
+
+
+@mem.cache
+def cached_func(dataset, a):
+    print("input:", dataset)
+    if a == 10:
+        return -1
+    return 10 * 10
+
+
+if __name__ == "__main__":
+    print(cached_func(HYDE, 3))
