27e6ac5d25a273959c67d9367a6833c828cc1014
Alexander Kuhn-Regnier <ahk114@ic.ac.uk>
2020-03-14 16:08:21 +0000

    Regenerate dry day periods using the currently defined threshold. Clarified source of threshold.

--- src/wildfires/data/datasets.py
+++ src/wildfires/data/datasets.py
@@ -140,6 +140,7 @@ repo_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pard
 repo = Repo(repo_dir)
 
 # Above this mm/h threshold, a day is a 'wet day'.
+# 0.1 mm per day, from Harris et al. 2014, as used in Forkel et al. 2018.
 MM_PER_HR_THRES = 0.1 / 24
 M_PER_HR_THRES = MM_PER_HR_THRES / 1000
 
