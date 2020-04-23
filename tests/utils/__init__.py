3a72c6a7bca0d703974ae33a9381b8cd6d78a7fc
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2020-04-23 12:04:16 +0100

    Checked available memory prior to dataset tests. Sorting of requirements.

--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 import pytest
+from psutil import virtual_memory
 
 from wildfires.data.datasets import data_is_available
 
+
+def available_memory_gb():
+    return virtual_memory().total / 1024 ** 3
+
+
 data_availability = pytest.mark.skipif(
-    not data_is_available(), reason="Data directory is unavailable."
+    not data_is_available() or available_memory_gb() < 10,
+    reason="Data directory is unavailable or there is not enough memory.",
 )
