657f92a6d60ee387430058d5a7d56a135d1ba914
Alexander Kuhn-Regnier <ahk114@ic.ac.uk>
2020-05-12 13:03:59 +0100

    Automatic Scheduler determination. Incorporated worker address into notebook listing output.

--- src/wildfires/dask_cx1/dask_utils.py
+++ src/wildfires/dask_cx1/dask_utils.py
@@ -7,18 +7,20 @@ from time import monotonic, sleep
 from dask.distributed import get_worker
 
 from ..ports import get_ports
-from .dask_cx1 import strip_multiline
+from .dask_cx1 import get_client, strip_multiline
 
 
 class NoNotebookError(RuntimeError):
     """Raised when a matching notebook could not be found within the timeout."""
 
 
-def get_running_procs(client, user="ahk114", workers=None):
+def get_running_procs(client=None, user="ahk114", workers=None):
     """Return a string containing `user`'s currently running processes.
 
     Args:
-        client (dask.distributed.Client): Client used to submit functions.
+        client (dask.distributed.Client): Client used to submit functions. If None,
+            `wildfires.dask_cx1.get_client()` will be used to attempt automatic
+            retrieval of an existing cluster.
         user (str): User for which to query running processes.
         workers (str): If given, will be used to define on which worker(s) the
             function should be run.
@@ -27,6 +29,8 @@ def get_running_procs(client, user="ahk114", workers=None):
         str: The running processes.
 
     """
+    if client is None:
+        client = get_client()
 
     def _get_running_procs():
         return check_output(shlex.split(f"pgrep -afu {user}")).decode()
@@ -35,7 +39,7 @@ def get_running_procs(client, user="ahk114", workers=None):
 
 
 def start_jupyter_lab(
-    client,
+    client=None,
     workers=None,
     jupyter_exc="/rds/general/user/ahk114/home/.pyenv/versions/wildfires/bin/jupyter",
     notebook_dir="~/Documents/PhD/wildfire-analysis/analyses/",
@@ -47,7 +51,9 @@ def start_jupyter_lab(
     This is followed by port forwarding to the target host.
 
     Args:
-        client (dask.distributed.Client): Client used to submit functions.
+        client (dask.distributed.Client): Client used to submit functions. If None,
+            `wildfires.dask_cx1.get_client()` will be used to attempt automatic
+            retrieval of an existing cluster.
         worker (str): This is used to set the worker the function should be run on.
         jupyter_exc (str): Path to the Jupyter executable to use.
         notebook_dir (str): The working directory.
@@ -59,6 +65,8 @@ def start_jupyter_lab(
         address: The JupyterLab worker address.
 
     """
+    if client is None:
+        client = get_client()
 
     def _start_jupyter_lab():
         port = get_ports()[0]
@@ -85,7 +93,7 @@ def start_jupyter_lab(
 
 
 def list_notebooks(
-    client,
+    client=None,
     jupyter_exc="/rds/general/user/ahk114/home/.pyenv/versions/wildfires/bin/jupyter",
     filter_port=None,
     workers=None,
@@ -94,22 +102,26 @@ def list_notebooks(
     """List notebooks currently running off of the Jupyter executable `jupyter_exc`.
 
     Args:
-        client (dask.distributed.Client): Client used to submit functions.
+        client (dask.distributed.Client): Client used to submit functions. If None,
+            `wildfires.dask_cx1.get_client()` will be used to attempt automatic
+            retrieval of an existing cluster.
         jupyter_exc (str): Path to the Jupyter executable to use.
         filter_port (int): If given, only notebooks running on 'localhost:`port`' will
             be listed.
-        workers (str): If given, will be used to define on which worker(s) the
-            function should be run.
+        workers (str): If given, will be used to define which worker(s) the function
+            should be run on.
         timeout (float or None): Timeout before we stop looking for a matching
             notebook. If `timeout` is None, the timeout is disabled.
 
     Returns:
-        str: The running notebook(s).
+        str: The running notebook(s) and the associated Dask worker address.
 
     Raises:
         NoNotebookError: If no matching notebook could not be found within the timeout.
 
     """
+    if client is None:
+        client = get_client()
 
     def _list_notebooks():
         notebooks = (
@@ -123,17 +135,22 @@ def list_notebooks(
                 for notebook in notebooks
                 if f"localhost:{filter_port}" in notebook
             ]
-        return "\n".join(notebooks)
+
+        if notebooks:
+            return "\n".join([f"Running on worker: {get_worker().address}", *notebooks])
 
     if timeout is None:
         timeout = math.inf
 
     start = monotonic()
     while (monotonic() - start) < timeout:
-        notebooks = client.submit(_list_notebooks, workers=workers, pure=False).result()
-        if len(notebooks.split("\n")) >= 1:
-            return notebooks
-        sleep(0.5)
+        notebook_listing = client.submit(
+            _list_notebooks, workers=workers, pure=False
+        ).result()
+        if notebook_listing is None:
+            sleep(0.5)
+            continue
+        return notebook_listing
     else:
         # We have timed out without finding a matching notebook.
         raise NoNotebookError(
@@ -143,7 +160,7 @@ def list_notebooks(
 
 
 def setup_remote_jupyter_lab(
-    client,
+    client=None,
     workers=None,
     jupyter_exc="/rds/general/user/ahk114/home/.pyenv/versions/wildfires/bin/jupyter",
     notebook_dir="~/Documents/PhD/wildfire-analysis/analyses/",
@@ -158,7 +175,9 @@ def setup_remote_jupyter_lab(
     JupyterLab environment and then retrieve its address and token.
 
     Args:
-        client (dask.distributed.Client): Client used to submit functions.
+        client (dask.distributed.Client): Client used to submit functions. If None,
+            `wildfires.dask_cx1.get_client()` will be used to attempt automatic
+            retrieval of an existing cluster.
         workers (str): This is used to set the worker the function should be run on.
         jupyter_exc (str): Path to the Jupyter executable to use.
         notebook_dir (str): The working directory.
@@ -166,9 +185,13 @@ def setup_remote_jupyter_lab(
         target_host (str): SSH hostname.
 
     Returns:
-        str: The running notebook(s) including their access tokens.
+        str: The running notebook(s) including their access tokens and the Dask worker
+            address.
 
     """
+    if client is None:
+        client = get_client()
+
     port, worker_address = start_jupyter_lab(
         client, workers, jupyter_exc, notebook_dir, target_user, target_host
     )
@@ -176,5 +199,4 @@ def setup_remote_jupyter_lab(
         # If no worker was explicitly requested, make sure that the notebooks are
         # listed on the worker JupyterLab is running on.
         workers = worker_address
-    notebooks = list_notebooks(client, jupyter_exc, port, workers)
-    return notebooks
+    return list_notebooks(client, jupyter_exc, port, workers)
