5d9e1dba9b6e6334f716fcd798e3e3e080b9e71a
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2020-11-05 09:16:06 +0000

    Removed erroneous 'rasterised' savefig kwarg.

--- src/wildfires/analysis/plotting.py
+++ src/wildfires/analysis/plotting.py
@@ -73,7 +73,6 @@ class FigureSaver:
     options = {
         "bbox_inches": "tight",
         "transparent": True,
-        "rasterised": True,
         "filetype": "pdf",
         "dpi": 600,
     }
@@ -81,7 +80,6 @@ class FigureSaver:
     debug_options = {
         "bbox_inches": "tight",
         "transparent": False,
-        "rasterised": True,
         "filetype": "png",
         "dpi": 350,
     }
